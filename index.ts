93b2377f25e363e1531c56f11fecbfa55027c598
Dasky <daskymail@gmail.com>
2026-01-01 13:06:08 +0100

    init

---
+++ index.ts
@@ -0,0 +1,21 @@
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const token = process.env.DISCORD_TOKEN;
+
+if (!token) {
+  throw new Error("DISCORD_TOKEN is not defined in environment variables.");
+}
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
